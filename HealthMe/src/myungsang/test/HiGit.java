03d445fb10aabc9003dc2731f3bc607e079fb73d
user1-PC\user1 <gokuorzzl@naver.com>
2018-10-02 14:45:07 +0900

    test1

---
+++ HealthMe/src/myungsang/test/HiGit.java
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+<script
+  src="https://code.jquery.com/jquery-3.3.1.js"
+  integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60="
+  crossorigin="anonymous"></script>
+    
+    
+<style>
+    .mslove{
+        font-size:200%;
+		color:red;
+    }
+</style>    
+<script>
+    $(document).ready(function(){
+        $('button').click(function(){
+            
+        });        
+    });
+</script>
+
+<img id="img" src="../img/sana.jpg"
+    style="width:500px; height:300px;"/>
+    <br>
+<button>정보 출력</button>
+    
+
+
+
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
