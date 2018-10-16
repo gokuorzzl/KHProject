076643d5afc00b44601e375d25c2805bbf1811a7
chodayeon <esa888@naver.com>
2018-10-16 11:56:52 +0900

    기존 필요한거 추가 파일

---
+++ PlusServerKHProject/healthMe/WebContent/js/custom.js
@@ -0,0 +1,40 @@
+(function ($) {
+
+  "use strict";
+
+    // 시간 설정함수
+    $(window).load(function(){
+      $('.preloader').fadeOut(2000); 
+    });
+
+
+    // MENU
+    $('.menu-burger').on('click', function() {
+      $('.menu-bg, .menu-items, .menu-burger').toggleClass('fs');
+      $('.menu-burger').text() == "☰" ? $('.menu-burger').text('✕') : $('.menu-burger').text('☰');
+    });
+
+
+    // 배경 슬라이드 속도 조절
+    $('body').vegas({
+          overlay: true,
+        transition: 'fade', 
+        transitionDuration: 4000,
+        delay: 7000,
+        animation: 'random',
+        animationDuration: 20000,
+        backgroundSize:'cover',
+        slides: [
+            { src: 'images/dayeon.png' },
+            { src: 'images/hyeyeon.png' },
+            { src: 'images/sola_bg.png' },
+            { src: 'images/jongkyu.png' },
+            { src: 'images/myeongsang.png' },
+            { src: 'images/yeonggi.png' },
+            { src: 'images/yongseon.png' },
+        ],
+        timer: true,
+        transition: [ 'zoomOut', ]
+    });
+
+})(jQuery);
