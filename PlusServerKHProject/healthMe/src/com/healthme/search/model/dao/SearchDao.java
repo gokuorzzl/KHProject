02a1434c825f4eda7b6ff8b50f25e8972d64ce26
user1-PC\user1 <go8476@naver.com>
2018-10-26 17:22:56 +0900

    커

--- PlusServerKHProject/healthMe/src/com/healthme/search/model/dao/SearchDao.java
+++ PlusServerKHProject/healthMe/src/com/healthme/search/model/dao/SearchDao.java
@@ -22,57 +22,34 @@ public class SearchDao {
 		ArrayList<SearchedTrainer> tmpMember = new ArrayList<>(); 
 		
 		//Member테이블로부터 id, name, score, event를 가져올 쿼리
-		String query = "SELECT MEMBERID, MEMBERNAME, TRAINERSCORE, TRAINEREVENT FROM MEMBER WHERE TRAINEREVENT = ? OR TRAINERREGION = ?";
+		String query = "SELECT MEMBERID, MEMBERNAME, TRAINERSCORE, TRAINEREVENT FROM MEMBER WHERE "; 
+		
+		//searchList의 검색어만큼 쿼리문 생성 
+		for(int i=0 ; i<searchList.size() ; i++) {
+			query += "TRAINEREVENT LIKE ? OR TRAINERREGION LIKE ? ";
+		}
+		
 		//사용자의 검색어는 종목 또는 지역이지만 단어만으로는 어떤 범주에 속하는지 알 수 없으므로 지역, 종목 두 필드 모두에 검색어를 넣어서 SELECT
 		
 		try {
 			
-			int index = 0; //리스트 내 검색어 이용을 위한 index
-			boolean check = false; //Id 중복검사를 위한 변수
-								//중복되는 아이디가 있는 경우 true, 중복되는 id가 없는 경우 false
-			SearchedTrainer trainer = new SearchedTrainer(); //결과를 담을 SearchedTrainer객체
+			//Connection 객체를 이용해 query문을 전송할 PreparedStatement객체 생성
+			pstmt = conn.prepareStatement(query);
 			
-			while(!searchList.isEmpty() && index<=searchList.size()) {//검색어 수만큼 DB 검색을 수행
-				
-				//Connection 객체를 이용해 query문을 전송할 PreparedStatement객체 생성
-				pstmt = conn.prepareStatement(query);
-				
-				//?에 들어갈 요소 지정
-				pstmt.setString(1, searchList.get(index)); //종목필드 검색
-				pstmt.setString(2, searchList.get(index)); //지역필드 검색
-				
-				//rsetMember에 결과 저장
-				rset = pstmt.executeQuery();
-				
-				//rset에 저장된 결과를 SearchedTrainer객체에 저장 후 ArrayList<SearchedTrainer>에 저장 
-				if(rset.next()) {
-					
-					//두 번 검색하므로 겹치는 ID가 있을경우 저장하지 않음
-					String memberId = rset.getString("MEMBERID"); //아이디를 먼저 저장
-					
-					for(int i=0 ; i<tmpMember.size() ; i++) {
-						if(memberId==tmpMember.get(i).getMemberId()) {
-							//새로 검색된 memberId와 tmpList내의 memberId가 같을 경우 결과를 담지 않음
-							check = true;
-							break;
-						}
-					}
-					
-					if(check==true) { //tmpList 내에 중복되는 ID가 존재하는 경우 저장하지 않음
-						check = false; //check 초기화
-						index++; //index값 증가
-						continue;
-					}else {
-						trainer.setMemberName(rset.getString("MEMBERNAME"));
-						trainer.setTrainerScore(rset.getString("TRAINERSCORE"));
-						trainer.setTrainerEvent(rset.getString("TRAINEREVENT"));
-						tmpMember.add(trainer); //ArrayList에 객체 추가 
-						check = false; //check 초기화
-						index++; //index값 증가
-					}
- 
-				}//rset.next() if문 종료
-			}//while문 종료
+			//?에 들어갈 요소 지정
+			int index = 0; //값을 저장하기 위해 index지정
+			for(int i=0 ; i<searchList.size()*2 ; i+=2) {
+				//searchList의 검색어 하나당 ?가 두 개이므로 두 배만큼 for문 실행
+				pstmt.setString(i, searchList.get(index));
+				pstmt.setString(i+1, searchList.get(index));
+				index++;//for문의 i와 별개로 searchList의 값을 가져오기 위함
+			}
+			
+			//query문 마무리
+			query += "ORDER BY TRAINERSCORE DESC";
+			System.out.println(query);
+			//쿼리문 전송 후 결과를 rset에 저장
+			rset = pstmt.executeQuery();
 			
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
@@ -81,7 +58,9 @@ public class SearchDao {
 			JDBCTemplate.close(rset);
 			JDBCTemplate.close(pstmt);
 		}
+		
 		return tmpMember;
+		
 	}//searchMemberInfo 메소드 종료
 	
 	public ArrayList<String> searchTrainerImg(Connection conn, ArrayList<SearchedTrainer> tmpMember) {
