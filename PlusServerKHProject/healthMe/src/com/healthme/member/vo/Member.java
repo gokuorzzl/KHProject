7c9699b500f5fe919527b351921315cddd29c686
SoraJung <jarasasa2@gmail.com>
2018-10-29 11:35:19 +0900

    Member vo/Member.java

--- PlusServerKHProject/healthMe/src/com/healthme/member/vo/Member.java
+++ PlusServerKHProject/healthMe/src/com/healthme/member/vo/Member.java
@@ -9,35 +9,41 @@ public class Member {
 	private String memberName;
 	private String memberSocialId;
 	private String memberAddr;
-	private Date memberRegistDate;
-	private String memberClass;
 	private String memberEmail;
 	private String memberPhone;
-	private String memberTrainer;
-	private String memberOut;
+	private char memberTrainer;
+	private String memberClass;
+	private char memberOut;
+	private Date memberRegistDate;
 	private Date memberOutDate;
-	private String trainerFileName;
-	private String trainerContent;
-	private String trainerUniversity;
-	private String trainerEvent;
-	private String trainerRegion;
-	private String trainerScore;
+
 	
-	public Member(String memberId, String memberPw) {
+	
+	  public Member(int pkMemberNumber, String memberId, String memberName, String memberEmail, String memberPhone) {
+	      super();
+	      this.pkMemberNumber = pkMemberNumber;
+	      this.memberId = memberId;
+	      this.memberName = memberName;
+	      this.memberEmail = memberEmail;
+	      this.memberPhone = memberPhone;      
+	      
+	   }
+
+	   public Member(String memberId, String memberPw) {
 		super();
 		this.memberId = memberId;
 		this.memberPw = memberPw;
 	}
+	
 
+	
 	public Member() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-
 	public Member(int pkMemberNumber, String memberId, String memberPw, String memberName, String memberSocialId,
-			String memberAddr, Date memberRegistDate, String memberClass, String memberEmail, String memberPhone,
-			String memberTrainer, String memberOut, Date memberOutDate, String trainerFileName, String trainerContent,
-			String trainerUniversity, String trainerEvent, String trainerRegion, String trainerScore) {
+			String memberAddr, String memberEmail, String memberPhone, char memberTrainer, String memberClass,
+			char memberOut, Date memberRegistDate, Date memberOutDate) {
 		super();
 		this.pkMemberNumber = pkMemberNumber;
 		this.memberId = memberId;
@@ -45,175 +51,93 @@ public class Member {
 		this.memberName = memberName;
 		this.memberSocialId = memberSocialId;
 		this.memberAddr = memberAddr;
-		this.memberRegistDate = memberRegistDate;
-		this.memberClass = memberClass;
 		this.memberEmail = memberEmail;
 		this.memberPhone = memberPhone;
 		this.memberTrainer = memberTrainer;
+		this.memberClass = memberClass;
 		this.memberOut = memberOut;
+		this.memberRegistDate = memberRegistDate;
 		this.memberOutDate = memberOutDate;
-		this.trainerFileName = trainerFileName;
-		this.trainerContent = trainerContent;
-		this.trainerUniversity = trainerUniversity;
-		this.trainerEvent = trainerEvent;
-		this.trainerRegion = trainerRegion;
-		this.trainerScore = trainerScore;
 	}
-
-
-
-
 	public int getPkMemberNumber() {
 		return pkMemberNumber;
 	}
-
 	public void setPkMemberNumber(int pkMemberNumber) {
 		this.pkMemberNumber = pkMemberNumber;
 	}
-
 	public String getMemberId() {
 		return memberId;
 	}
-
 	public void setMemberId(String memberId) {
 		this.memberId = memberId;
 	}
-
 	public String getMemberPw() {
 		return memberPw;
 	}
-
 	public void setMemberPw(String memberPw) {
 		this.memberPw = memberPw;
 	}
-
 	public String getMemberName() {
 		return memberName;
 	}
-
 	public void setMemberName(String memberName) {
 		this.memberName = memberName;
 	}
-
 	public String getMemberSocialId() {
 		return memberSocialId;
 	}
-
 	public void setMemberSocialId(String memberSocialId) {
 		this.memberSocialId = memberSocialId;
 	}
-
 	public String getMemberAddr() {
 		return memberAddr;
 	}
-
 	public void setMemberAddr(String memberAddr) {
 		this.memberAddr = memberAddr;
 	}
-
-	public Date getMemberRegistDate() {
-		return memberRegistDate;
-	}
-
-	public void setMemberRegistDate(Date memberRegistDate) {
-		this.memberRegistDate = memberRegistDate;
-	}
-
-	public String getMemberClass() {
-		return memberClass;
-	}
-
-	public void setMemberClass(String memberClass) {
-		this.memberClass = memberClass;
-	}
-
 	public String getMemberEmail() {
 		return memberEmail;
 	}
-
 	public void setMemberEmail(String memberEmail) {
 		this.memberEmail = memberEmail;
 	}
-
 	public String getMemberPhone() {
 		return memberPhone;
 	}
-
 	public void setMemberPhone(String memberPhone) {
 		this.memberPhone = memberPhone;
 	}
-
-	public String getMemberTrainer() {
+	public char getMemberTrainer() {
 		return memberTrainer;
 	}
-
-	public void setMemberTrainer(String memberTrainer) {
+	public void setMemberTrainer(char memberTrainer) {
 		this.memberTrainer = memberTrainer;
 	}
-
-	public String getMemberOut() {
+	public String getMemberClass() {
+		return memberClass;
+	}
+	public void setMemberClass(String memberClass) {
+		this.memberClass = memberClass;
+	}
+	public char getMemberOut() {
 		return memberOut;
 	}
-
-	public void setMemberOut(String memberOut) {
+	public void setMemberOut(char memberOut) {
 		this.memberOut = memberOut;
 	}
-
+	public Date getMemberRegistDate() {
+		return memberRegistDate;
+	}
+	public void setMemberRegistDate(Date memberRegistDate) {
+		this.memberRegistDate = memberRegistDate;
+	}
 	public Date getMemberOutDate() {
 		return memberOutDate;
 	}
-
 	public void setMemberOutDate(Date memberOutDate) {
 		this.memberOutDate = memberOutDate;
 	}
 
-	public String getTrainerFileName() {
-		return trainerFileName;
-	}
-
-	public void setTrainerFileName(String trainerFileName) {
-		this.trainerFileName = trainerFileName;
-	}
-
-	public String getTrainerContent() {
-		return trainerContent;
-	}
-
-	public void setTrainerContent(String trainerContent) {
-		this.trainerContent = trainerContent;
-	}
-
-	public String getTrainerUniversity() {
-		return trainerUniversity;
-	}
-
-	public void setTrainerUniversity(String trainerUniversity) {
-		this.trainerUniversity = trainerUniversity;
-	}
-
-	public String getTrainerEvent() {
-		return trainerEvent;
-	}
-
-	public void setTrainerEvent(String trainerEvent) {
-		this.trainerEvent = trainerEvent;
-	}
-
-	public String getTrainerRegion() {
-		return trainerRegion;
-	}
-
-	public void setTrainerRegion(String trainerRegion) {
-		this.trainerRegion = trainerRegion;
-	}
-
-	public String getTrainerScore() {
-		return trainerScore;
-	}
-
-	public void setTrainerScore(String trainerScore) {
-		this.trainerScore = trainerScore;
-	}
 	
 
 
